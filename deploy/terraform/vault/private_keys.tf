c71d01506b9a4a15f16be7f0816e17eae65bcdb1
Denis <korbakovd@gmail.com>
2026-05-08 01:10:57 +0300

    fix: update rotation

--- deploy/terraform/vault/private_keys.tf
+++ deploy/terraform/vault/private_keys.tf
@@ -5,4 +5,8 @@ resource "time_rotating" "sso-service-key-rotation" {
 resource "tls_private_key" "sso-service-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
+
+  lifecycle {
+    replace_triggered_by = [time_rotating.sso-service-key-rotation]
+  }
 }
